c0ad2e14a3d73de81e1e1542348d1dbfdd2e3876
Harsh Agrawal <agrawalharsh199925@gmail.com>
2021-09-15 16:57:38 +0530

    completed till 15th video

--- src/App.js
+++ src/App.js
@@ -27,11 +27,13 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode  has been enabled", "success")
+      document.title = "textutils - dark mode"
     }
     else{
       setMode('light')
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode  has been enabled", "success")
+      document.title = "textutils - light mode"
     }
   }
   return (
